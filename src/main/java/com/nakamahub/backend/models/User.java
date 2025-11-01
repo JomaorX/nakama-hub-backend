98cf647ee9dbd49f2b3dcac55cde834123f830cb
Jose Miguel <martinez97.pro@gmail.com>
2025-11-01 12:12:17 +0100

    [ADD] User table defined

--- src/main/java/com/nakamahub/backend/models/User.java
+++ src/main/java/com/nakamahub/backend/models/User.java
@@ -1,4 +1,31 @@
 package com.nakamahub.backend.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Builder.Default
+    private UserRole role = UserRole.ROLE_USER;
+
 }
