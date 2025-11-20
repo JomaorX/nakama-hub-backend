a5983e15ce15414875e4f2cf5871d08c3eb0abe2
Jose Miguel <martinez97.pro@gmail.com>
2025-11-20 20:00:17 +0100

    [ADD] comment controller and createComment method

---
+++ src/main/java/com/nakamahub/backend/controllers/CommentController.java
@@ -0,0 +1,26 @@
+package com.nakamahub.backend.controllers;
+
+import com.nakamahub.backend.dtos.CommentResponseDTO;
+import com.nakamahub.backend.dtos.CreateCommentDTO;
+import com.nakamahub.backend.services.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/comment")
+public class CommentController {
+    @Autowired
+    CommentService commentService;
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public CommentResponseDTO createComment (@RequestBody CreateCommentDTO newComment){
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return commentService.createComment(newComment, username);
+    }
+
+}
